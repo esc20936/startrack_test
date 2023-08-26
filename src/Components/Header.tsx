8d7361125590b152802e93cd4b57a3afc2357b95
Pablo Escobar <esc20936@uvg.edu.gt>
2023-08-25 20:43:16 -0600

    feat(): initial setup and header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+import Logo from '@/assets/logo/logo.svg'
+
+export default function Header() {
+  return (
+    <header className='flex flex-col items-center w-full mt-[57px]' >
+        <Image src={Logo} alt="logo" />
+    </header>
+  )
+}
