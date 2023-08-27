cb4c90b7ddc45475618d3cb0ae7e97cef09cad67
Pablo Escobar <esc20936@uvg.edu.gt>
2023-08-26 21:33:14 -0600

    fix(): bar should only act as a button if not showing content

--- src/Components/LikedSection/LikedSection.tsx
+++ src/Components/LikedSection/LikedSection.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Loader from "../Loader/Loader";
 import Image from "next/image";
 import smallHeart from "@/assets/small-heart/small-heart.svg";
+import mediumHeart from "@/assets/medium-heart/medium-heart.svg";
 import arrowUp from "@/assets/arrow-up/arrow-up.svg";
 
 interface Props {
@@ -23,8 +24,10 @@ export default function LikedSection({ loading }: Props) {
     <>
       <div className={
         showLiked ?
-        "w-full h-[74px] bg-darkPurple bg-opacity-[28%] rounded-t-[16px] p-[16px] border-b-0 border-solid border-[1px] border-cardLoader border-opacity-[44%] flex flex-row justify-between cursor-pointer":
-        "w-full h-[74px] bg-cardLoader bg-opacity-[28%] rounded-[16px] p-[16px] border-solid border-[1px] border-cardLoader border-opacity-[44%] flex flex-row justify-between cursor-pointer"}>
+        "w-full h-[74px] bg-darkPurple bg-opacity-[28%] rounded-t-[16px] p-[16px] border-b-0 border-solid border-[1px] border-cardLoader border-opacity-[44%] flex flex-row justify-between":
+        "w-full h-[74px] bg-cardLoader bg-opacity-[28%] rounded-[16px] p-[16px] border-solid border-[1px] border-cardLoader border-opacity-[44%] flex flex-row justify-between cursor-pointer"}
+        onClick={() => showLiked ? "" : setShowLiked(true)}
+        >
         {/* icon and name */}
         <div className="flex flex-row items-center gap-[8px]">
           {/* path */}
@@ -48,8 +51,14 @@ export default function LikedSection({ loading }: Props) {
 
       <div className={
           showLiked ?
-          'w-full h-[167px] bg-darkPurple border-t-0 rounded-b-[16px] p-[16px] border-solid border-[1px] border-cardLoader border-opacity-[44%] flex flex-row justify-between duration-300':
-          'w-full h-[0px] bg-darkPurple  border-t-0 rounded-b-[16px] p-[16px] border-solid border-[1px] border-cardLoader border-opacity-[44%] flex flex-row justify-between duration-300 transform -translate-y-1  opacity-0 -z-10'}></div>
+          'w-full h-[167px] bg-darkPurple border-t-0 rounded-b-[16px] p-[16px] border-solid border-[1px] border-cardLoader border-opacity-[44%] flex flex-row justify-between items-center duration-300':
+          'w-full h-[0px] bg-darkPurple  border-t-0 rounded-b-[16px] p-[16px] border-solid border-[1px] border-cardLoader border-opacity-[44%] flex flex-row justify-between duration-300 transform -translate-y-1  opacity-0 -z-10'}>
+
+            <div className="w-full h-full flex flex-col items-center justify-center">
+                <Image src={mediumHeart} alt="small heart" className="h-[34px] w-[33px] mb-[15px]" />
+              <h1 className="text-white font-primary_Medium text-[15px] text-center">You haven’t liked any superhero yet</h1>
+            </div>
+          </div>
     </>
   );
 }
